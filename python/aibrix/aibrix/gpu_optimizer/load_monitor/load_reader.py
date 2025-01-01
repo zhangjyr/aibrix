28b69ebaff29f3f8ae356e42519d0b06d4565d95
Jingyuan Zhang <jingyuan.zhang0929@bytedance.com>
2025-01-01 13:09:36 -0800

    Use current ts to load profile may to early, revert to use an interval ago.

--- python/aibrix/aibrix/gpu_optimizer/load_monitor/load_reader.py
+++ python/aibrix/aibrix/gpu_optimizer/load_monitor/load_reader.py
@@ -245,7 +245,9 @@ class GatewayLoadReader:
                 return []
 
             # TODO: Now profile seems to be have a interval delay. Further investigation is needed.
-            profiles = self.read_key(f"{self.prefix}{int(ts)}", True)
+            profiles = self.read_key(
+                f"{self.prefix}{int(ts - self.key_ts_alignment)}", True
+            )
             self.last_ts = ts
 
             if profiles is None or len(profiles) == 0:

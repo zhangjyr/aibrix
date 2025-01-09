444ca30f68b3b2f7c07788ec8a7a49b9e3f29ee3
Jingyuan Zhang <jingyuan.zhang0929@bytedance.com>
2025-01-09 15:50:13 -0800

    Bug fix

--- python/aibrix/aibrix/gpu_optimizer/optimizer/profiling/gpu_benchmark.py
+++ python/aibrix/aibrix/gpu_optimizer/optimizer/profiling/gpu_benchmark.py
@@ -247,13 +247,14 @@ async def send_request(
             if len(token_latencies) == 0
             else len(token_latencies) + 1,
             "timestamp": request_start_time,
-            "E2E": request_latency,
-            "TTFT": time_to_first,
-            "TPOT_mean": np.mean(token_latencies),
-            "TPOT_P50": np.percentile(token_latencies, 50),
-            "TPOT_P90": np.percentile(token_latencies, 90),
-            "TPOT_P99": np.percentile(token_latencies, 99),
+            "E2E": request_latency
         }
+        if len(token_latencies) > 0:
+            request_trace["TTFT"] = time_to_first
+            request_trace["TPOT_mean"] = np.mean(token_latencies)
+            request_trace["TPOT_P50"] = np.percentile(token_latencies, 50)
+            request_trace["TPOT_P90"] = np.percentile(token_latencies, 90)
+            request_trace["TPOT_P99"] = np.percentile(token_latencies, 99)
         print(json.dumps(request_trace))
     REQUEST_LATENCY.append((prompt_len, output_len, request_latency))
     if len(token_latencies) > 0:
